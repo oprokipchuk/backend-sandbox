4566b5ce6ffec640c7c7e4fb35fe26030d1ebba0
OlegAgrus <34576707+OlegAgrus@users.noreply.github.com>
2023-09-25 18:41:14 +0300

    add typo to check the CI build correctness

--- domainless/hello-world/src/main/java/com/github/oprokipchuk/helloworld/HelloWorldApplication.java
+++ domainless/hello-world/src/main/java/com/github/oprokipchuk/helloworld/HelloWorldApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class, args);
+        SpringgApplication.run(HelloWorldApplication.class, args);
     }
 }
