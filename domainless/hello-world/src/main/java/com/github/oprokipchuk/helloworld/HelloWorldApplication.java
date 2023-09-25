57c0644dc468fa790aa4c7dbd257df43201181b6
OlegAgrus <34576707+OlegAgrus@users.noreply.github.com>
2023-09-25 18:42:26 +0300

    Revert "add typo to check the CI build correctness"
    This reverts commit 4566b5ce6ffec640c7c7e4fb35fe26030d1ebba0.

--- domainless/hello-world/src/main/java/com/github/oprokipchuk/helloworld/HelloWorldApplication.java
+++ domainless/hello-world/src/main/java/com/github/oprokipchuk/helloworld/HelloWorldApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
-        SpringgApplication.run(HelloWorldApplication.class, args);
+        SpringApplication.run(HelloWorldApplication.class, args);
     }
 }
